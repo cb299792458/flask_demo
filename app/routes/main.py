040ad19174c591350439f503835ef32578aab8cb
brian <brianrlam@gmail.com>
2023-07-12 19:27:48 -0400

    create session to track views

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,18 +1,38 @@
-from flask import Blueprint,render_template,redirect
+from flask import Blueprint,render_template,redirect,session
 from app.sample_form import SampleForm
 
 bp=Blueprint('',__name__)
 
+def track_views():
+    if 'views' in session:
+        session['views']=session.get('views')+1
+    else:
+        session['views']=1
+
+
 @bp.route('/')
 def index():
+    track_views()
     return render_template('page.html', title='Welcome!')
 
 
 @bp.route('/help')
 def help():
+    track_views()
     return render_template('page.html', title='Help!')
 
-    
+
+@bp.route('/views')
+def views():
+    return f'Total Views: {session.get("views")}'
+
+
+@bp.route('/views/reset')
+def reset_views():
+    views = session.pop('views',None)
+    return f'Reset Views (previously {views})'
+
+
 @bp.route('/form', methods=['GET', 'POST'])
 def form():
     # load form from Jinja template
