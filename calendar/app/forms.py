7f22277289cc3641e823d4a7c1a0584db54c781b
brian <brianrlam@gmail.com>
2023-07-13 12:12:28 -0400

    finish calendar

--- calendar/app/forms.py
+++ calendar/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import BooleanField, DateField, StringField, SubmitField, TextAreaField, TimeField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from datetime import datetime
 
 class AppointmentForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -11,3 +12,12 @@ class AppointmentForm(FlaskForm):
     description = TextAreaField('Description', validators=[DataRequired()])
     private = BooleanField('Private?')
     submit = SubmitField('Create Appointment')
+
+    # will auto run on validate_on_submit
+    def validate_end_date(form,field):
+        start = datetime.combine(form.start_date.data,form.start_time.data)
+        end = datetime.combine(form.end_date.data,form.end_time.data)
+        if start > end:
+            raise ValidationError("End date/time must come after start date/time")
+        if form.start_date.data!=form.end_date.data:
+            raise ValidationError("Event must start and end on same day")
--- calendar/app/routes.py
+++ calendar/app/routes.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, url_for
 import os
 import psycopg2
 from app.forms import AppointmentForm
-from datetime import datetime
+from datetime import datetime, timedelta
 
 CONNECTION_PARAMETERS = {
     "user": os.environ.get("DB_USER"),
@@ -16,6 +16,15 @@ bp=Blueprint('main',__name__,'/')
 
 @bp.route('/', methods=['GET', 'POST'])
 def main():
+    d=datetime.now()
+    return redirect(url_for(".daily", year=d.year, month=d.month, day=d.day))
+
+@bp.route('/<int:year>/<int:month>/<int:day>', methods=['GET', 'POST'])
+def daily(year,month,day):
+    day=datetime(year=year,month=month,day=day)
+    next_day=day+timedelta(days=1)
+
+
     form=AppointmentForm()
     if form.validate_on_submit():
         params = {
@@ -31,11 +40,9 @@ def main():
                     """
                     INSERT INTO appointments (name, start_datetime, end_datetime, description, private)
                     VALUES (%(name)s, %(start_datetime)s, %(end_datetime)s, %(description)s, %(private)s);
-                    """,
-                    params
+                    """, params
                 )
         return redirect('/')
-
 
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as curs:
@@ -43,8 +50,9 @@ def main():
                 """
                 SELECT id, name, start_datetime, end_datetime
                 FROM appointments
+                WHERE start_datetime BETWEEN %(day)s AND %(next_day)s
                 ORDER BY start_datetime;
-                """
+                """, {'day': day, 'next_day': next_day}
             )
             rows = curs.fetchall()
 
